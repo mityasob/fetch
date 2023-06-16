6688e8d2331bc906909d8529c1f06df274de1268
lokkie12 <31001852+lokkie12@users.noreply.github.com>
2023-06-16 23:16:29 +0300

    fix1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const repList = document.createElement("ul");
+repList.classList.add("rep-list");
+const repInfoList = document.querySelector(".rep-info-list");
+const hint = document.createElement("div");
+hint.classList.add("hint");
+const input = document.querySelector(".search-field");
+
 const debounce = (fn, debounceTime, value) => {
   let timer = this.lastCall;
   this.lastCall = Date.now();
@@ -9,76 +16,82 @@ const debounce = (fn, debounceTime, value) => {
   }, debounceTime);
 };
 
-let repList = document.querySelector(".rep-list");
-let repInfoList = document.querySelector(".rep-info-list");
+class NoSuchRepository extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NoSuchRepository";
+  }
+}
 
-let arr;
-let search = function (url) {
+const search = function (url) {
   let query = fetch(`https://api.github.com/search/repositories?q=${url}`);
   query
     .then((res) => {
+      if (res.status >= 400) {
+        throw new Error("Error. Try again");
+      }
       return res.json();
     })
     .then((res) => {
-      arr = res.items.slice(0, 5);
-      arr.forEach((element, index) => {
-        repList.children[index].setAttribute(
-          "style",
-          "padding-left: 13px; padding-bottom: 9px; border: 2px solid black;"
-        );
-        repList.children[index].textContent = element.name;
+      let arr = res.items.slice(0, 5);
+      if (arr.length === 0) {
+        throw new NoSuchRepository("No Such Repository");
+      }
+      input.after(repList);
+      arr.forEach((el, index) => {
+        let rep = document.createElement("li");
+        rep.classList.add("rep");
+        repList.appendChild(rep);
+        rep.textContent = el.name;
+        rep.addEventListener("click", () => {
+          let li = document.createElement("li");
+          li.classList.add("rep-info");
+          li.innerHTML =
+            "<p>Name: " +
+            arr[index].name +
+            "</p><p>Owner: " +
+            arr[index].owner.login +
+            "</p><p>Stars: " +
+            arr[index].stargazers_count;
+          let closeButton = document.createElement("div");
+          closeButton.classList.add("close-button");
+          li.appendChild(closeButton);
+          let firstStick = document.createElement("div");
+          firstStick.classList.add("first-stick");
+          closeButton.appendChild(firstStick);
+          let secondStick = document.createElement("div");
+          secondStick.classList.add("second-stick");
+          closeButton.appendChild(secondStick);
+          closeButton.addEventListener("click", () => {
+            li.remove();
+          });
+          repInfoList.appendChild(li);
+          debounce(clearInput, 500, repList);
+        });
       });
       repInfoList.removeAttribute("style");
+    })
+    .catch((err) => {
+      hint.textContent = err.message;
+      input.after(hint);
+      debounce(clearInput, 500, repList);
     });
 };
 
-let clearInput = function (list) {
+const clearInput = function (list) {
   input.value = "";
-  for (let el of list) {
-    el.removeAttribute("style");
-    el.textContent = "";
-  }
+  list.innerHTML = "";
   repInfoList.setAttribute("style", "transform: translate(0px, 240px);");
 };
 
-let input = document.querySelector(".search-field");
 input.addEventListener("input", () => {
   if (input.value === " ") {
     input.value = "";
   }
   if (input.value) {
+    hint.remove();
     debounce(search, 500, input.value);
   } else {
-    debounce(clearInput, 500, repList.children);
+    debounce(clearInput, 500, repList);
   }
-});
-
-let repListArr = Array.from(repList.children);
-
-repListArr.forEach((el, index) => {
-  el.addEventListener("click", () => {
-    let li = document.createElement("li");
-    li.classList.add("rep-info");
-    li.innerHTML =
-      "<p>Name: " +
-      arr[index].name +
-      "</p><p>Owner: " +
-      arr[index].owner.login +
-      "</p><p>Stars: " +
-      arr[index].stargazers_count;
-    let closeButton = document.createElement("div");
-    closeButton.classList.add("close-button");
-    li.appendChild(closeButton);
-    let firstStick = document.createElement("div");
-    firstStick.classList.add("first-stick");
-    closeButton.appendChild(firstStick);
-    let secondStick = document.createElement("div");
-    secondStick.classList.add("second-stick");
-    closeButton.appendChild(secondStick);
-    closeButton.addEventListener("click", () => {
-      li.remove();
-    });
-    repInfoList.appendChild(li);
-    debounce(clearInput, 500, repList.children);
-  });
 });
